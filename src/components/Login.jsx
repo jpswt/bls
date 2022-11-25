e4c22f93bdc7905c6fd3f98eb2fec23ef9fc429a
jpswt <joel_swt@yahoo.com>
2022-11-24 22:40:51 -0500

    fix border

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 const Login = () => {
 	return (
-		<div className="flex flex-col h-full justify-center items-center">
+		<div className="relative flex flex-col h-full justify-center items-center">
 			<h1>Login</h1>
-			<form className="flex flex-col border-r2 bg-slate-100 w-[20rem] h-[10rem]">
-				<input type="text" placeholder="Username" className="m-2" />
-				<input type="password" placeholder="Password" className="m-2" />
+			<form className="flex flex-col bg-gray-300 w-[20rem] h-[15rem] p-6 rounded-md border-teal-600 border-2 ">
+				<input type="text" placeholder="Username" className="m-4 px-4 py-2" />
+				<input
+					type="password"
+					placeholder="Password"
+					className="m-4 px-4 py-2"
+				/>
 				<button>Login</button>
 			</form>
 		</div>
