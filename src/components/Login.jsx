0f396eb6de025aaf0d668fd43efbd3628a18c8d8
jpswt <joel_swt@yahoo.com>
2022-11-26 18:08:17 -0500

    login css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,12 +4,16 @@ const Login = () => {
 	return (
 		<div className="relative flex flex-col h-full justify-center items-center">
 			<h1>Login</h1>
-			<form className="flex flex-col bg-gray-300 w-[20rem] h-[15rem] p-6 rounded-md border-teal-600 border-2 ">
-				<input type="text" placeholder="Username" className="m-4 px-4 py-2" />
+			<form className="flex flex-col bg-white w-[20rem] h-[15rem] p-6 rounded-md border-teal-600 border-2 ">
+				<input
+					type="text"
+					placeholder="Username"
+					className="m-4 px-4 py-2 bg-slate-200"
+				/>
 				<input
 					type="password"
 					placeholder="Password"
-					className="m-4 px-4 py-2"
+					className="m-4 px-4 py-2 bg-slate-200"
 				/>
 				<button>Login</button>
 			</form>
